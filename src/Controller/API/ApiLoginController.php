1b6160771a5474204c11e15a715f8d4439d568f6
ntsanq <ntsanq@gmail.com>
2022-06-13 17:47:09 +0700

    feat: create login and logout function

---
+++ src/Controller/API/ApiLoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'app_api_login')]
+    public function index(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $token = 'thisIsToken';
+        return $this->json([
+            'user' => $user->getUserIdentifier(),
+            'token' => $token,
+        ]);
+    }
+}
