f6faab2fe20dbb4ef8c269711330236bf1063975
Tuan <ductuan.tran@nfq.asia>
2022-06-28 22:39:08 +0700

    feat: add event fail
    add event fail

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
+    {
+    }
+}
