bb5752c60c2cb5bcd75cbe81fb9940fc7d8a1791
tolehoai <tolehoai@gmail.com>
2022-06-29 15:44:41 +0700

    fix: add CONST

--- src/Repository/FlightRepository.php
+++ src/Repository/FlightRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Airline;
 use App\Entity\Airplane;
 use App\Entity\Flight;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -18,6 +19,10 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class FlightRepository extends BaseRepository
 {
+    const FLIGHT_ALIAS = 'f';
+    const AIRLINE_ALIAS = 'al';
+    const AIRPLANE_ALIAS = 'ap';
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Flight::class);
@@ -25,34 +30,46 @@ class FlightRepository extends BaseRepository
 
     public function filter(array $listFlightRequest): array
     {
-        $qb = $this->createQueryBuilder('p');
-        $qb->join(Airplane::class, 'ap', \Doctrine\ORM\Query\Expr\Join::WITH, 'p.airplane = ap.id');
-        $qb->join(Airline::class, 'al', \Doctrine\ORM\Query\Expr\Join::WITH, 'ap.airline=al.id');
+        $qb = $this->createQueryBuilder(self::FLIGHT_ALIAS);
+        $qb->join(Airplane::class, self::AIRPLANE_ALIAS, Join::WITH, self::FLIGHT_ALIAS . '.airplane =' . self::AIRPLANE_ALIAS . '.id');
+        $qb->join(Airline::class, self::AIRLINE_ALIAS, Join::WITH, self::AIRPLANE_ALIAS . '.airline=' . self::AIRLINE_ALIAS . '.id');
         if (!empty($listFlightRequest['criteria']['startTime'])) {
             $listFlightRequest['like']['startTime'] = $listFlightRequest['criteria']['startTime'];
             unset($listFlightRequest['criteria']['startTime']);
         }
-        foreach ($listFlightRequest['criteria'] as $key => $value) {
-            if ($value != null) {
-                $qb->andWhere('p.' . $key . ' = ' . '\'' . $value . '\'');
-            }
-        }
-        foreach ($listFlightRequest['like'] as $key => $value) {
-            if ($value != null) {
-                $qb->andWhere('p.' . $key . ' LIKE ' . '\'' . $value . '%\'');
-            }
-        }
-
-//        if (!isset($listCarRequest['filterBy'])) {
-//            $query = $qb->getQuery();
-//            return $query->getResult();
-//        }
-//        foreach ($listCarRequest['filterBy'] as $key => $value) {
-//            $qb->addOrderBy('p.' . $key, $value);
-//        }
-
+        $this->addWhere($listFlightRequest, $qb);
+        $this->addLike($listFlightRequest, $qb);
         $query = $qb->getQuery();
 
         return $query->getResult();
+    }
+
+    private function addWhere($listFlightRequest, $qb)
+    {
+        foreach ($listFlightRequest['criteria'] as $key => $value) {
+            if ($value != null) {
+                $qb->andWhere(self::FLIGHT_ALIAS . '.' . $key . ' = ' . '\'' . $value . '\'');
+            }
+        }
+    }
+
+    private function addLike($listFlightRequest, $qb)
+    {
+        foreach ($listFlightRequest['like'] as $key => $value) {
+            if ($value != null) {
+                $qb->andWhere(self::FLIGHT_ALIAS . '.' . $key . ' LIKE ' . '\'' . $value . '%\'');
+            }
+        }
+    }
+
+    private function sort($listCarRequest, $qb)
+    {
+        if (!isset($listCarRequest['filterBy'])) {
+            $query = $qb->getQuery();
+            return $query->getResult();
+        }
+        foreach ($listCarRequest['filterBy'] as $key => $value) {
+            $qb->addOrderBy(self::FLIGHT_ALIAS . '.' . $key, $value);
+        }
     }
 }
