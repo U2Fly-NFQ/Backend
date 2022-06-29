d8b2aec4bb8f037afe3b03c7b9ff349ce7d7ea2a
Ductuan92 <101399418+Ductuan92@users.noreply.github.com>
2022-06-29 16:13:53 +0700

    feat: list all airline (#7)
    list all airline

---
+++ src/Controller/AirlineController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AirlineRepository;
+use App\Traits\JsonTrait;
+use App\Transformer\AirlineTransformer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AirlineController extends AbstractController
+{
+    use JsonTrait;
+
+    #[Route('/api/airlines', 'app_list_airline', methods: 'GET')]
+    public function list(AirlineRepository $airlineRepository, AirlineTransformer $airlineTransformer)
+    {
+        $airlines = $airlineRepository->findAll();
+        $data = [];
+        foreach ($airlines as $airline) {
+            $data[] = $airlineTransformer->objectToArray($airline);
+        }
+
+        return $this->success($data);
+    }
+}
