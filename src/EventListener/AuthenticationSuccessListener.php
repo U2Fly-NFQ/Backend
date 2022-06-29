e460bdf013ca128e1f50ab3520bc0db0ccbfb113
ntsanq <ntsanq@gmail.com>
2022-06-29 10:24:57 +0700

    feat: login by email

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
+    {
+
+    }
+}
---
+++ src/Traits/JsonTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+trait JsonTrait
+{
+    protected function success(array $data, int $status = Response::HTTP_OK, array $header = []): JsonResponse
+    {
+        $dataResponse = [
+            'status' => 'success',
+            'data' => $data
+        ];
+        return new JsonResponse($dataResponse, $status, $header);
+    }
+
+    protected function failed(string $message, int $status = Response::HTTP_BAD_REQUEST, array $header = []): JsonResponse
+    {
+        $dataResponse = [
+            'status' => 'failed',
+            'data' => $message
+        ];
+        return new JsonResponse($dataResponse, $status, $header);
+    }
+
+    protected function error($errors, int $status = Response::HTTP_BAD_REQUEST, array $header = []): JsonResponse
+    {
+        $errorArray = [];
+
+        foreach ($errors as $error) {
+            $errorArray[$error->getPropertyPath()] = $error->getMessage();
+        }
+
+        $dataResponse = [
+            'status' => 'error',
+            'data' => $errorArray
+        ];
+        return new JsonResponse($dataResponse, $status, $header);
+    }
+}
