9574843a319edbab626848cb679e979e3c6f72de
Tuan <ductuan.tran@nfq.asia>
2022-06-29 14:26:35 +0700

    fix: fix endpoints
    fix endpoints

--- src/Controller/AirplaneController.php
+++ src/Controller/AirplaneController.php
@@ -3,21 +3,17 @@
 namespace App\Controller;
 
 use App\Repository\AirplaneRepository;
-use App\Request\AirplaneRequest;
 use App\Traits\JsonTrait;
 use App\Transformer\AirplaneTransformer;
-use PhpParser\JsonDecoder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AirplaneController extends AbstractController
 {
     use JsonTrait;
 
-    #[Route('/airplane', name: 'app_list_airplane', methods: 'GET')]
+    #[Route('/api/airplanes', name: 'app_list_airplane', methods: 'GET')]
     public function list(AirplaneRepository $airplaneRepository, AirplaneTransformer $airplaneTransformer): JsonResponse
     {
         $airplanes = $airplaneRepository->findAll();
