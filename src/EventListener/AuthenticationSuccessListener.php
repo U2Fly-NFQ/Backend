d61fdd0565b6955e3ae11f41335bbfe11afa4b18
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-06-29 13:57:40 +0700

    fix: change return data value

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -9,16 +9,17 @@ class AuthenticationSuccessListener
 {
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
     {
-        $data = $event->getData();
         $account = $event->getUser();
         if (!$account instanceof UserInterface) {
             return;
         }
-        $data['data'] = [
+        $data = [
             'id'=>$account->getId(),
             'username'=>$account->getUserIdentifier(),
-            'roles' => $account->getRoles()
+            'roles' => $account->getRoles(),
         ];
+        $data = array_merge($data,$event->getData());
+
         $event->setData($data);
     }
 }
