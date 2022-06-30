66418e8db9b49da2ec9ab6c07ad3746ba263ba6d
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-06-30 23:50:27 +0700

    feat: find flight by id (#23)

---
+++ src/Controller/Flight/FindFlightController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Flight;
+
+use App\Repository\FlightRepository;
+use App\Traits\JsonTrait;
+use App\Transformer\FlightTransformer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api', name: 'api_flight')]
+class FindFlightController
+{
+    use JsonTrait;
+
+    #[Route('/flights/{id}', name: 'find', methods: 'GET')]
+    public function findById(
+        int               $id,
+        FlightRepository  $flightRepository,
+        FlightTransformer $flightTransformer,
+    ): JsonResponse
+    {
+        $flight = $flightRepository->find($id);
+        if ($flight == null) {
+            return $this->error([]);
+        }
+        $data = $flightTransformer->toArray($flight);
+
+        return $this->success($data);
+    }
+}
