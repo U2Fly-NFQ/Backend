90b9bb1deb1a76cd9df16d936ce7d12ddda8ece9
Tuan <ductuan.tran@nfq.asia>
2022-06-30 20:48:13 +0700

    feat: find discount by id
    find discount by id

---
+++ src/Controller/Discount/DiscountsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Discount;
+
+use App\Entity\Discount;
+use App\Repository\DiscountRepository;
+use App\Traits\JsonTrait;
+use App\Transformer\DiscountTransformer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DiscountsController extends AbstractController
+{
+    use JsonTrait;
+
+    #[Route('/api/discounts/{id}', name: 'app_discounts', methods: 'GET')]
+    public function findById(int $id, DiscountRepository $discountRepository, DiscountTransformer $discountTransformer): JsonResponse
+    {
+        $discount = $discountRepository->find($id);
+        $data = $discountTransformer->toArray($discount);
+        
+        return $this->success($data);
+    }
+}
