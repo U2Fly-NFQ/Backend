6b8b004f63ac4ab10d2031297d8b0ce79e236392
Tuan <ductuan.tran@nfq.asia>
2022-06-30 22:16:42 +0700

    fix: add passenger
    add passenger

--- src/Transformer/AccountTransformer.php
+++ src/Transformer/AccountTransformer.php
@@ -2,17 +2,19 @@
 
 namespace App\Transformer;
 
+use App\Constant\DatetimeConstant;
 use App\Entity\Account;
 
 class AccountTransformer extends AbstractTransformer
 {
     const BASE_ATTRIBUTE = ['id', 'email'];
+    const PASSENGER_ATTRIBUTE = ['id','name', 'gender', 'address', 'identification'];
 
     public function toArray(Account $account): array
     {
         $result = $this->transform($account, self::BASE_ATTRIBUTE);
-        $result['passengerId'] = $account->getPassenger()->getId();
-
-        return $result;
+        $passenger = $this->transform($account->getPassenger(), self::PASSENGER_ATTRIBUTE);
+        $passenger['birthday'] = $account->getPassenger()->getBirthday()->format(DatetimeConstant::DATETIME_DEFAULT);
+        return array_merge($result, $passenger);
     }
 }
