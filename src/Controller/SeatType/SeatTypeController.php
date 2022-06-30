96ce45e9eba842adbcf4c94bd77c549a9622202b
Tuan <ductuan.tran@nfq.asia>
2022-06-30 21:14:44 +0700

    feat: find seatType by id
    find seatType by id

---
+++ src/Controller/SeatType/SeatTypeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\SeatType;
+
+use App\Entity\SeatType;
+use App\Repository\SeatTypeRepository;
+use App\Traits\JsonTrait;
+use App\Transformer\SeatTypeTransformer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SeatTypeController extends AbstractController
+{
+    use JsonTrait;
+
+    #[Route('/api/seattype/{id}', name: 'app_seat_type', methods: 'GET')]
+    public function findById(int $id, SeatTypeTransformer $seatTypeTransformer, SeatTypeRepository $seatTypeRepository): JsonResponse
+    {
+        $discount = $seatTypeRepository->find($id);
+        if($discount == null){
+            return $this->error([]);
+        }
+        $data = $seatTypeTransformer->toArray($discount);
+
+        return $this->success($data);
+    }
+}
---
+++ src/Transformer/SeatTypeTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Transformer;
+
+use App\Constant\DatetimeConstant;
+use App\Entity\SeatType;
+
+class SeatTypeTransformer extends AbstractTransformer
+{
+    const BASE_ATTRIBUTE = ['id', 'name'];
+
+    public function toArray(SeatType $seatType): array
+    {
+        $result = $this->transform($seatType, self::BASE_ATTRIBUTE);
+        $result['createdAt'] = $seatType->getCreatedAt()->format(DatetimeConstant::DATETIME_DEFAULT);
+        if(!empty($seatType->getUpdatedAt())){
+            $result['updatedAt'] = $seatType->getUpdatedAt()->format(DatetimeConstant::DATETIME_DEFAULT);
+        }
+
+        return $result;
+    }
+}
