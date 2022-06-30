292f2ace509c8b7e8b113375ca57740cecaae5e7
Ductuan92 <101399418+Ductuan92@users.noreply.github.com>
2022-06-30 13:53:23 +0700

    Airplane CRUD (#15)

---
+++ src/Constant/DatetimeConstant.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Constant;
+
+class DatetimeConstant
+{
+    public const DATETIME_DEFAULT = 'Y-m-d H:i:s';
+}
--- src/Transformer/AirlineTransformer.php
+++ src/Transformer/AirlineTransformer.php
@@ -2,22 +2,24 @@
 
 namespace App\Transformer;
 
+use App\Constant\DatetimeConstant;
 use App\Entity\Airline;
+use PHP_CodeSniffer\Tests\Core\Tokenizer\DoubleArrowTest;
 
-class AirlineTransformer
+class AirlineTransformer extends AbstractTransformer
 {
+    const BASE_ATTRIBUTE = ['id', 'name', 'icao'];
+
     /**
      * @param Airline $airline
      * @return array
      */
     public function objectToArray(Airline $airline): array
     {
-        return [
-            'airlineId' => $airline->getId(),
-            'airlineName' => $airline->getName(),
-            'airlineIcao' => $airline->getIcao(),
-            'createdDate' => $airline->getUpdatedAt()->format(date(DATE_ATOM)),
-            'updateDate' => $airline->getUpdatedAt()->format(date(DATE_ATOM))
-        ];
+        $result = $this->transform($airline, self::BASE_ATTRIBUTE);
+        $result['createdAt'] = $airline->getCreatedAt()->format(DatetimeConstant::DATETIME_DEFAULT);
+        $result['updatedAt'] = $airline->getUpdatedAt()->format(DatetimeConstant::DATETIME_DEFAULT);
+
+        return $result;
     }
 }
