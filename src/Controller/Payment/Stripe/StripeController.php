754c7baaf766d0d083586d10d1c6369570c5d5c4
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-07-04 01:30:27 +0700

    Make payment (#46)

---
+++ src/Controller/Payment/Stripe/StripeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Payment\Stripe;
+
+use App\Request\PaymentRequest;
+use App\Service\StripeService;
+use App\Traits\JsonTrait;
+use Stripe\Exception\ApiErrorException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/payment', name: 'api_stripe_')]
+class StripeController extends AbstractController
+{
+
+    use JsonTrait;
+
+    /**
+     * @throws ApiErrorException
+     */
+    #[Route('/stripe', name: 'pay')]
+    public function pay(
+        StripeService $stripeService,
+        Request $request,
+        PaymentRequest $paymentRequest,
+    )
+    {
+        $requestBody = json_decode($request->getContent(), true);
+        $paymentRequest = $paymentRequest->fromArray($requestBody);
+        $checkout = $stripeService->getPayment($paymentRequest);
+
+        return $this->success(['checkoutURL' => $checkout->url]);
+    }
+}
