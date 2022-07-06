a5e82a3c169eb21e410f1089d5db803743287954
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-07-06 08:21:35 +0700

    fix: parameter bag

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -6,15 +6,16 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class MailService
 {
     const SUPPORT_MAIL_NAME = 'U2Fly Support';
-    private ContainerBagInterface $containerBag;
+    private ParameterBagInterface $parameterBag;
 
-    public function __construct(ContainerBagInterface $containerBag)
+    public function __construct(ParameterBagInterface $parameterBag)
     {
-        $this->containerBag = $containerBag;
+        $this->parameterBag = $parameterBag;
     }
 
     public function mail($userMail, string $path, $userName = 'User'): void
@@ -23,19 +24,20 @@ class MailService
         try {
             $mail->SMTPDebug = SMTP::DEBUG_OFF;
             $mail->isSMTP();
-            $mail->Host = $this->containerBag->get('zohoHost');
+            $mail->Host = $this->parameterBag->get('zohoHost');
             $mail->SMTPAuth = true;
-            $mail->Username = $this->containerBag->get('zohoMail');
-            $mail->Password = $this->containerBag->get('zohoPassword');
+            $mail->Username = $this->parameterBag->get('zohoMail');
+            $mail->Password = $this->parameterBag->get('zohoPassword');
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
-            $mail->setFrom($this->containerBag->get('zohoMail'), self::SUPPORT_MAIL_NAME);
+            $mail->setFrom($this->parameterBag->get('zohoMail'), self::SUPPORT_MAIL_NAME);
             $mail->addAddress($userMail, $userName);
 
             $mail->isHTML(true);
-            $mail->Subject = "Hello $userName";
+            $mail->Subject = "Successfully payment for $userName";
             $mail->Body = file_get_contents($path);
+
             $mail->send();
         } catch (Exception $e) {
             throw new \Exception("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
