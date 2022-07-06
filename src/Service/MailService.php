73858fb26ba590ae14d7b64601cfb58862b06afc
tolehoai <69009588+tolehoai@users.noreply.github.com>
2022-07-06 10:58:02 +0700

    fix: send mail (#67)

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -11,36 +11,36 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 class MailService
 {
     const SUPPORT_MAIL_NAME = 'U2Fly Support';
-    private ParameterBagInterface $parameterBag;
+    private ContainerBagInterface $containerBag;
 
-    public function __construct(ParameterBagInterface $parameterBag)
+    public function __construct(ContainerBagInterface $containerBag)
     {
-        $this->parameterBag = $parameterBag;
+        $this->containerBag = $containerBag;
     }
 
     public function mail($userMail, string $path, $userName = 'User'): void
     {
         $mail = new PHPMailer(true);
+
         try {
             $mail->SMTPDebug = SMTP::DEBUG_OFF;
             $mail->isSMTP();
-            $mail->Host = $this->parameterBag->get('zohoHost');
+            $mail->Host = $this->containerBag->get('zohoHost');
             $mail->SMTPAuth = true;
-            $mail->Username = $this->parameterBag->get('zohoMail');
-            $mail->Password = $this->parameterBag->get('zohoPassword');
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Username = $this->containerBag->get('zohoMail');
+            $mail->Password = $this->containerBag->get('zohoPassword');
+            $mail->SMTPSecure = PHPMAILER::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
-            $mail->setFrom($this->parameterBag->get('zohoMail'), self::SUPPORT_MAIL_NAME);
+            $mail->setFrom($this->containerBag->get('zohoMail'), self::SUPPORT_MAIL_NAME);
             $mail->addAddress($userMail, $userName);
-
             $mail->isHTML(true);
             $mail->Subject = "Successfully payment for $userName";
             $mail->Body = file_get_contents($path);
-
             $mail->send();
         } catch (Exception $e) {
             throw new \Exception("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
         }
+
     }
 }
