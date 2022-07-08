af2cbff3e1b4799681c7b51aab5edadb8e284813
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-07-08 13:16:02 +0700

    fix: datetime in passenger birthday

--- src/Transformer/PassengerTransformer.php
+++ src/Transformer/PassengerTransformer.php
@@ -12,13 +12,16 @@ class PassengerTransformer extends AbstractTransformer
 
     const BASE_ATTRIBUTE = ['accountId', 'name', 'gender', 'birthday', 'address', 'identification'];
 
+    /**
+     * @throws \Exception
+     */
     public function toArray(Passenger $passenger): array
     {
         $result = $this->transform($passenger, self::BASE_ATTRIBUTE);
         $result['accountId'] = $passenger->getId();
         $passenger->getName() == null ? $result['name'] == null : $result['name'] = $passenger->getName();
         $passenger->getGender() == null ? $result['gender'] == null : $result['name'] = $passenger->getGender();
-        $passenger->getBirthday() == null ? $result['birthday'] == null : $result['birthday'] = $this->dateTimeToDate($passenger->getBirthday());
+        $passenger->getBirthday() == null ? $result['birthday'] == null : $result['birthday'] = new \DateTime($this->dateTimeToDate($passenger->getBirthday())) ;
         $passenger->getAddress() == null ? $result['address'] == null : $result['address'] = $passenger->getAddress();
         $passenger->getIdentification() == null ? $result['identification'] == null : $result['identification'] = $passenger->getIdentification();
         return $result;
