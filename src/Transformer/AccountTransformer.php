81e07376ddf4a84bae5054f2b59fe19639f15d91
tolehoai <tolehoai@gmail.com>
2022-07-08 11:41:53 +0700

    fix: remove comment

--- src/Transformer/AccountTransformer.php
+++ src/Transformer/AccountTransformer.php
@@ -8,7 +8,6 @@ use App\Entity\Account;
 class AccountTransformer extends AbstractTransformer
 {
     const BASE_ATTRIBUTE = ['id', 'email'];
-//    const PASSENGER_ATTRIBUTE = ['id', 'name', 'gender', 'address', 'identification', 'birthday'];
     private PassengerTransformer $passengerTransformer;
     public function __construct(PassengerTransformer $passengerTransformer)
     {
