31d5b3f9704f8cc7782baeb75c2f0e6c47b11440
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-07-10 03:12:17 +0700

    update: timezone

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+date_default_timezone_set('%env(APP_TIMEZONE)%');
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
