d150d8141416732c401a006bda422a9f5740e8d7
tolehoai <69009588+tolehoai@users.noreply.github.com>
2022-07-10 02:46:01 +0700

    Be 231 ticket statistic (#112)

--- src/Controller/Payment/Stripe/SuccessStripeController.php
+++ src/Controller/Payment/Stripe/SuccessStripeController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Payment\Stripe;
 
 use App\Constant\StripeConstant;
-use App\Event\BookingEvent;
+use App\Event\BaseEvent;
 use App\Event\MailerEvent;
 use App\Service\TicketFlightService;
 use App\Service\TicketService;
@@ -45,7 +45,7 @@ class SuccessStripeController
 
         $mailerEvent = new MailerEvent($ticket);
         $eventDispatcher->dispatch($mailerEvent, 'event.successMail');
-        $bookingEvent = new BookingEvent();
+        $bookingEvent = new BaseEvent();
         $eventDispatcher->dispatch($bookingEvent, 'event.bookingSuccess');
 
         return new RedirectResponse($parameterBag->get('returnTicketUrl') . '/' . $ticket->getId());
---
+++ src/Event/BaseEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class BaseEvent extends Event
+{
+
+}
