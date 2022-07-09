ec2c149cad950d9b989ab300ac33234049402d82
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-07-09 16:51:15 +0700

    update: migrations

--- src/Repository/RoutesStatisticRepository.php
+++ src/Repository/RoutesStatisticRepository.php
@@ -14,53 +14,10 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method RoutesStatistic[]    findAll()
  * @method RoutesStatistic[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class RoutesStatisticRepository extends ServiceEntityRepository
+class RoutesStatisticRepository extends BaseRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, RoutesStatistic::class);
     }
-
-    public function add(RoutesStatistic $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(RoutesStatistic $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return RoutesStatistic[] Returns an array of RoutesStatistic objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?RoutesStatistic
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
--- src/Repository/TicketsStatisticRepository.php
+++ src/Repository/TicketsStatisticRepository.php
@@ -14,53 +14,10 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method TicketsStatistic[]    findAll()
  * @method TicketsStatistic[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class TicketsStatisticRepository extends ServiceEntityRepository
+class TicketsStatisticRepository extends BaseRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TicketsStatistic::class);
     }
-
-    public function add(TicketsStatistic $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(TicketsStatistic $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return TicketsStatistic[] Returns an array of TicketsStatistic objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?TicketsStatistic
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
