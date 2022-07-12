ddc47ead93168b687c3814b9b30f196fd3aec9ef
Ductuan92 <101399418+Ductuan92@users.noreply.github.com>
2022-07-12 15:12:58 +0700

    fix: TicketTransformer (#133)

--- tests/Transformer/TicketTransformerTest.php
+++ tests/Transformer/TicketTransformerTest.php
@@ -4,72 +4,66 @@ namespace App\Tests\Transformer;
 
 use App\Entity\Account;
 use App\Entity\Discount;
+use App\Entity\Flight;
 use App\Entity\Passenger;
 use App\Entity\SeatType;
 use App\Entity\Ticket;
+use App\Entity\TicketFlight;
+use App\Repository\SeatTypeRepository;
+use App\Repository\TicketFlightRepository;
 use App\Transformer\FlightTransformer;
 use App\Transformer\PassengerTransformer;
 use App\Transformer\TicketFlightTransformer;
 use App\Transformer\TicketTransformer;
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
 use JetBrains\PhpStorm\ArrayShape;
 use PHPUnit\Framework\TestCase;
 
 class TicketTransformerTest extends TestCase
 {
     /**
-     * @dataProvider getTicketDataProvider
      * @return void
      */
-    public function testToArray($param): void
-    {
-        $ticketTransformer = $this->createTicketTransformer();
-        $result = $ticketTransformer->toArray($param);
-        $this->assertIsArray($result);
-    }
-
-    /**
-     * @dataProvider getListTicketDataProvider
-     * @return void
-     */
-    public function testToArrayList($param): void
-    {
-        $ticketTransformer = $this->createTicketTransformer();
-        $result = $ticketTransformer->toArrayList($param);
-        $this->assertIsArray($result);
-    }
-
-
-    /**
-     * @return \App\Entity\Ticket[][][]
-     */
-    #[ArrayShape(['happy-case-1' => "\App\Entity\Ticket[][]"])]
-    public function getListTicketDataProvider()
-    {
-        $ticket1 = $this->createTicket(1);
-        $ticket2 = $this->createTicket(2);
-        $listTickets = [$ticket1,$ticket2];
-
-        return [
-            'happy-case-1'=> [
-                'param'=>$listTickets
-            ]
-        ];
-    }
-
-    /**
-     * @return \App\Entity\Ticket[][]
-     */
-    #[ArrayShape(['happy-case-1' => "\App\Entity\Ticket[]"])]
-    public function getTicketDataProvider(): array
+    public function testToArray(): void
     {
         $ticket = $this->createTicket();
+        $ticketTransformer = $this->createTicketTransformer();
+        $result = $ticketTransformer->toArray($ticket);
 
-        return [
-            'happy-case-1'=> [
-                'param'=>$ticket
-            ]
-        ];
+        $this->assertIsArray($result);
+    }
+
+    /**
+     * @return void
+     */
+    public function testToArrayList(): void
+    {
+        $ticket = $this->createTicket();
+        $ticketTransformer = $this->createTicketTransformer();
+        $result = $ticketTransformer->toArrayList([$ticket]);
+        $this->assertIsArray($result);
+    }
+
+    /**
+     * @return Ticket|\PHPUnit\Framework\MockObject\MockObject
+     */
+    public function createTicket(): Ticket|\PHPUnit\Framework\MockObject\MockObject
+    {
+        $date = new DateTime('2020-1-1');
+        $account = $this->getMockBuilder(Account::class)->disableOriginalConstructor()->getMock();
+        $passenger = $this->getMockBuilder(Passenger::class)->disableOriginalConstructor()->getMock();
+        $passenger->expects($this->any())->method('getAccount')->willReturn($account);
+        $ticketFlight = $this->getMockBuilder(Ticket::class)->disableOriginalConstructor()->getMock();
+        $ticketFlights = new ArrayCollection([$ticketFlight]);
+        $ticket = $this->getMockBuilder(Ticket::class)->disableOriginalConstructor()->getMock();
+        $ticket->expects($this->any())->method('getTicketFlights')->willReturn($ticketFlights);
+        $ticket->expects($this->any())->method('getPassenger')->willReturn($passenger);
+        $seatType = $this->getMockBuilder(SeatType::class)->disableOriginalConstructor()->getMock();
+        $ticket->expects($this->any())->method('getSeatType')->willReturn($seatType);
+        $ticket->expects($this->any())->method('getUpdatedAt')->willReturn($date);
+
+        return $ticket;
     }
 
     /**
@@ -77,38 +71,16 @@ class TicketTransformerTest extends TestCase
      */
     private function createTicketTransformer(): TicketTransformer
     {
+        $flight = $this->getMockBuilder(Flight::class)->disableOriginalConstructor()->getMock();
+        $ticketFlight = $this->getMockBuilder(TicketFlight::class)->disableOriginalConstructor()->getMock();
+        $ticketFlight->expects($this->any())->method('getFlight')->willReturn($flight);
         $passengerTransformer = $this->getMockBuilder(PassengerTransformer::class)->disableOriginalConstructor()->getMock();
         $flightTransformer = $this->getMockBuilder(FlightTransformer::class)->disableOriginalConstructor()->getMock();
         $ticketFlightTransformer = $this->getMockBuilder(TicketFlightTransformer::class)->disableOriginalConstructor()->getMock();
-        return new TicketTransformer($passengerTransformer, $flightTransformer, $ticketFlightTransformer);
-    }
+        $ticketFlightRepository = $this->getMockBuilder(TicketFlightRepository::class)->disableOriginalConstructor()->getMock();
+        $ticketFlightRepository->expects($this->any())->method('findBy')->willReturn([$ticketFlight]);
+        $seatTypeRepository = $this->getMockBuilder(SeatTypeRepository::class)->disableOriginalConstructor()->getMock();
 
-    /**
-     * @param int $id
-     * @return Ticket
-     */
-    private function createTicket(int $id = 1): Ticket
-    {
-
-        $passenger = new Passenger();
-        $account = $this->getMockBuilder(Account::class)->disableOriginalConstructor()->getMock();
-        $account->expects($this->any())->method('getEmail')->willReturn('sang@nfq.asia');
-        $passenger->setAccount($account);
-        $discount = new Discount();
-        $seatType = new SeatType();
-        $seatType->setName('business');
-        $passenger->setId('1');
-        $ticket = new Ticket();
-        $ticket->setId($id);
-        $ticket->setStatus(0);
-        $ticket->setDiscount($discount);
-        $ticket->setPassenger($passenger);
-        $ticket->setPaymentId('s231');
-        $ticket->setSeatType($seatType);
-        $ticket->setStatus(1);
-        $date = new DateTime();
-        $ticket->setUpdatedAt($date);
-
-        return $ticket;
+        return new TicketTransformer($passengerTransformer, $flightTransformer, $ticketFlightTransformer, $ticketFlightRepository, $seatTypeRepository);
     }
 }
